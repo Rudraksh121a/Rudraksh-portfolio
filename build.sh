5729de3808a9c2111cabb51c8177f62d32bfc089
Rudraksh121a <141481238+Rudraksh121a@users.noreply.github.com>
2024-10-27 18:49:38 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,23 +8,6 @@ pip install -r requirements.txt
 
 
 
-# Collect static files
-echo "Collecting static files..."
-python manage.py collectstatic --no-input
 
-# Run migrations
-echo "Applying database migrations..."
-python manage.py migrate
-
-# Create superuser if environment variable is set
-if [[ -n $CREATE_SUPERUSER && -n $DJANGO_SUPERUSER_EMAIL ]]; then
-  echo "Checking for superuser existence..."
-  if ! python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='$DJANGO_SUPERUSER_EMAIL').exists()"; then
-    echo "Creating superuser..."
-    python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
-  else
-    echo "Superuser with email $DJANGO_SUPERUSER_EMAIL already exists."
-  fi
-fi
 
 echo "Deployment complete."
